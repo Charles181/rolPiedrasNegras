dde2d48bd2e4cec1ba4fdebdf38eae8824333321
Carlos Santillan <csantillanj@gmail.com>
2025-12-19 12:47:13 -0600

    Initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,11 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+import tailwindVite from '@tailwindcss/vite';
+
+// https://astro.build/config
+export default defineConfig({
+  vite: {
+    plugins: [tailwindVite()]
+  }
+});
