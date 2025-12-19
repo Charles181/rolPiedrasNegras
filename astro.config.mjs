45f55d4462b88030d5fa7bd38afc0c655ac11059
Carlos Santillan <csantillanj@gmail.com>
2025-12-19 12:57:01 -0600

    added netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import netlify from '@astrojs/netlify';
 
 import tailwindVite from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
+  adapter: netlify(),
   vite: {
     plugins: [tailwindVite()]
   }
